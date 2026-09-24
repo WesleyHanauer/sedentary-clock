c46f845f7389b5dc616cac1058f790db865bdd0b
WesleyHanauer <wesleyhanauer@gmail.com>
2026-09-24 16:04:49 -0300

    implement basic styling for the segmented selection

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,8 +1,9 @@
 const regularTimerForm = document.getElementById("regularTimerForm");
 const exerciseTimerForm = document.getElementById("exerciseTimerForm");
 const info = document.getElementById("info");
-const exerciseTimerRadio = document.getElementById("exerciseTimer");
-const regularTimerRadio = document.getElementById("regularTimer");
+const exerciseLabel = document.getElementById("exerciseLabel");
+const regularLabel = document.getElementById("regularLabel");
+
 
 regularTimerForm.style.display = "none";
 
@@ -16,7 +17,7 @@ const radioButtons = document.querySelectorAll("input[name=timerSelect]");
 
 radioButtons.forEach(radioButton => {
     radioButton.addEventListener("change", (event) => {
-        selectChange(event.target.value);
+        selectChange(event.target.id);
     })
 });
 
@@ -25,7 +26,12 @@ function selectChange(type){
         regularTimerForm.style.display = "flex";
         exerciseTimerForm.style.display = "none";
         info.style.display = "none";
+        regularLabel.classList.add("bg-sky-700");
+        exerciseLabel.classList.remove("bg-sky-700");
+        
     } else if (type == "exerciseTimer") {
+        exerciseLabel.classList.add("bg-sky-700");
+        regularLabel.classList.remove("bg-sky-700");
         exerciseTimerForm.style.display = "flex";
         regularTimerForm.style.display = "none";
         info.style.display = "flex";
